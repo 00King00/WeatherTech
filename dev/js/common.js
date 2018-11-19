ece1865f0819b6bd252c0df9d0f23ddac41298fc
Coopert <kupchynskyi@gmail.com>
2018-11-19 11:21:30 +0200

    add header menu

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,6 +1,16 @@
 $(function(){
-	$('#masonry-item').masonry({
-		itemSelector: '.masonry-item',
-		columnWidth: 100
-	});
+	// $('#masonry-item').masonry({
+	// 	itemSelector: '.masonry-item',
+	// 	columnWidth: 100
+	// });
+
+	//burder buttons and menu
+	$('.bars').click(function(){
+		console.log(1);
+		$(this).toggleClass('active');
+		$(".menu").animate(
+			{height: 'toggle'},
+			{duration:500}
+		);
+	})
 })
